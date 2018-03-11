3963ce7c5dd2a00cf7efb708be10ed20e9ba5a91
Ali Siddiqui <alinsiddiqui@gmail.com>
2018-03-10 19:13:43 -0800

    Add implementation for Access-Control-Allow-Origin

--- web/pi_server.py
+++ web/pi_server.py
@@ -1,84 +1,33 @@
 from datetime import timedelta
 from functools import update_wrapper
 
-from flask import Flask, jsonify, current_app, request, make_response
+from flask import Flask, jsonify
 app = Flask(__name__)
 verification_required = True
 
-def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
-                attach_to_all=True, automatic_options=True):
-    """Decorator function that allows crossdomain requests.
-      Courtesy of
-      https://blog.skyred.fi/articles/better-crossdomain-snippet-for-flask.html
-    """
-    if methods is not None:
-        methods = ', '.join(sorted(x.upper() for x in methods))
-    if isinstance(max_age, timedelta):
-        max_age = max_age.total_seconds()
-
-    def get_methods():
-        """ Determines which methods are allowed
-        """
-        if methods is not None:
-            return methods
-
-        options_resp = current_app.make_default_options_response()
-        return options_resp.headers['allow']
-
-    def decorator(f):
-        """The decorator function
-        """
-        def wrapped_function(*args, **kwargs):
-            """Caries out the actual cross domain code
-            """
-            if automatic_options and request.method == 'OPTIONS':
-                resp = current_app.make_default_options_response()
-            else:
-                resp = make_response(f(*args, **kwargs))
-            if not attach_to_all and request.method != 'OPTIONS':
-                return resp
-
-            h = resp.headers
-            h['Access-Control-Allow-Origin'] = origin
-            h['Access-Control-Allow-Methods'] = get_methods()
-            h['Access-Control-Max-Age'] = str(max_age)
-            h['Access-Control-Allow-Credentials'] = 'true'
-            h['Access-Control-Allow-Headers'] = \
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-            if headers is not None:
-                h['Access-Control-Allow-Headers'] = headers
-            return resp
-
-        f.provide_automatic_options = False
-        return update_wrapper(wrapped_function, f)
-    return decorator
-
-
 
 @app.route('/')
-@crossdomain(origin='*')
 def f():
     return 'Raspberry Pi App running'
 
 @app.route('/mode')
-@crossdomain(origin='*')
 def get_status():
 	# Return verify or photos
 	resp = 'verify' if verification_required else 'photos'
+    resp = jsonify(resp)
+    resp.headers.add('Access-Control-Allow-Origin', '*')
 
 	if verification_required:
 		verification_required = False
-	print(resp)
+
 	return resp
-	
+
 @app.route('/code')
-@crossdomain(origin='*')
 def get_code():
 	return '999 136'
 
 
 @app.route('/get_images')
-@crossdomain(origin='*')
 def get_images():
 	sample_data = {
 		'images': [
