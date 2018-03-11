64a4fad299b191362e1a06d8a37309b5b285c860
Ali Siddiqui <alinsiddiqui@gmail.com>
2018-03-10 18:59:14 -0800

    Change port to 8080

--- web/pi_server.py
+++ web/pi_server.py
@@ -1,35 +1,35 @@
 from flask import Flask, jsonify
 app = Flask(__name__)
+verification_requred = True
 
 @app.route('/')
 def f():
-    return 'np'
+    return 'Raspberry Pi App running'
 
 @app.route('/mode')
-def g():
+def get_status():
 	# Return verify or photos
-	return 'verify'
+	return 'verify' if verification_requred else 'photos'
 
 @app.route('/code')
-def h():
-	# Return verify or photos
+def get_code():
 	return '999 136'
 
 
 @app.route('/get_images')
-def a():
+def get_images():
 	sample_data = {
 		'images': [
             {
-                'img': '',
-                'alt': ''
+                'img': 'https://i.pinimg.com/originals/62/20/d2/6220d255154fad0c911a3cb4c0072031.jpg'
 			},
 			{
-                'img': '',
-			    'alt': ''
+                'img': 'https://i.pinimg.com/originals/8b/a1/01/8ba101bc0e6fb061e79bef8c7bac97cc.jpg'
 			}
-		]}
+		]
+    }
+
 	return jsonify(sample_data)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=80)
+	app.run(host='0.0.0.0', port=8080)
