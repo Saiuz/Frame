af65fe76f59d0c450566166c093b71e56a4b2498
mattpaletta <mattpaletta@gmail.com>
2018-03-10 16:06:03 -0800

    Updated react app

---
+++ web/frame/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Carousel from './Carousel.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Carousel />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ web/frame/src/Carousel.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Slider from 'react-slick';
+import PropTypes from 'prop-types';
+
+class Carousel extends React.Component {
+	
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 100,
+	  autoplay: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+	let imageToShow;
+	if (this.props.images) {
+		imageToShow = this.props.images.map(img => {
+			return (<div><img src={img.src} alt={img.alt}></img></div>)
+		});
+	 }
+	return (
+		<Slider {...settings}>
+			{imageToShow}
+		</Slider>
+	);
+  }
+}
+
+Carousel.propTypes = {
+	img: Proptypes.object
+}
+
+export default Carousel;
