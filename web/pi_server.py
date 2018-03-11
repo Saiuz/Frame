d302da4f3a34a5370f95ac3f9100d2fcf7cdd581
Ali Siddiqui <alinsiddiqui@gmail.com>
2018-03-10 18:46:50 -0800

    Clean up server code

--- web/pi_server.py
+++ web/pi_server.py
@@ -5,30 +5,31 @@ app = Flask(__name__)
 def f():
     return 'np'
 
-@app.route("/mode")
+@app.route('/mode')
 def g():
 	# Return verify or photos
-	return "verify"
+	return 'verify'
 
-@app.route("/code")
+@app.route('/code')
 def h():
 	# Return verify or photos
-	return "999 136"
+	return '999 136'
 
 
-@app.route("/get_images")
+@app.route('/get_images')
 def a():
 	sample_data = {
-		"images": [
-			{ "img": "",
-			 "alt": "",
+		'images': [
+            {
+                'img': '',
+                'alt': ''
 			},
-			{ "img": "",
-			 "alt": "",
+			{
+                'img': '',
+			    'alt': ''
 			}
-			
 		]}
 	return jsonify(sample_data)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=80)
