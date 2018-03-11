01ea23df9b074a165699e89378c0420abf84622c
mattpaletta <mattpaletta@gmail.com>
2018-03-10 17:07:26 -0800

    Update web app

---
+++ web/frame/src/Verify.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from "axios";
+import "./Verify.css"
+
+class Verify extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {code: "999 136"};
+		let refreshInterval = 10000;
+		
+		//let codeUpdate = setInterval(this.getCode.bind(this), refreshInterval);
+	}
+	
+	getCode() {
+		axios.get("http://localhost/code").then(response => {
+            console.log(response);
+            this.setState({ code: response.data });
+        }).catch(response => {
+            // Do nothing if no server found.
+        });
+	}
+	
+  render() {
+	return (
+		<div className="verification-code-grid">
+			<div className="verification-code-display">
+				<h1>Verification Code</h1>
+				<h2>{this.state.code}</h2>
+			</div>
+		</div>
+	);
+  }
+}
+
+Verify.propTypes = {
+	code: PropTypes.object
+}
+
+export default Verify;
